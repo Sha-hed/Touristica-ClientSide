338a6069ca77278f612d3f5a38a5f86c681c7308
Sha-hed <shahedcse14@gmail.com>
2024-04-25 21:39:36 +0600

    Footer Page Added

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Navbar";
+import Footer from "../pages/Footer";
 
 const Root = () => {
     return (
         <div>
-           <h1>Mike Check Mike Check!</h1> 
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
