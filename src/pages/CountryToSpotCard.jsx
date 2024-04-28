2c580fd8a35c903b4da573f43aeabf98fcafd7c8
Sha-hed <shahedcse14@gmail.com>
2024-04-28 20:10:06 +0600

    View Details Page Added For Spots Card

--- src/pages/CountryToSpotCard.jsx
+++ src/pages/CountryToSpotCard.jsx
@@ -6,7 +6,9 @@ const CountryToSpotCard = ({country}) => {
             <div className="w-96 bg-base-100 shadow-xl rounded-t-0">
                 <figure><img className="rounded-t-0" src={country.photo} /></figure>
                 <div className="p-2 flex flex-col">
-                    <h2 className="card-title my-2">{country.spot_name}, {country.country_name}</h2>
+                    <h2 className="card-title mt-2">{country.spot_name}, {country.country_name}</h2>
+                    <h3 className="font-medium text-lg">Location : {country.location}</h3>
+                    <h3 className="font-medium">Average Cost : {country.average_cost} tk,  <span className="ml-2">Seasonality : {country.season}</span></h3>
                     <p className="">
                         {country.description.length > 200 ? country.description.slice(0, 200) : country.description}
                     </p>
